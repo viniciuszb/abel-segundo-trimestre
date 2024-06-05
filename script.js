bbe37d5b6936c2c73ee4ea520c87e3a535eafeb2
viniciuszb <161598083+viniciuszb@users.noreply.github.com>
2024-06-05 09:45:35 -0300

    Add files via upload
    05/06

---
+++ script.js
@@ -0,0 +1,16 @@
+const caixaPrincipal = document.querySelector(".caixa-pricipal")
+const caixaPerguntas = document.querySelector(".caixa-perguntas")
+const caixaAlternativas = document.querySelector(".caixa-alternativa")
+const caixaResultado = document.querySelector(".caixa-resultado")
+const textoResultado = document.querySelector*(".texto-resultado")
+
+const perguntas = [
+{
+    enunciado: "pergunta1"
+    alternativas: ["alternativa1", "alternativas2"]
+},
+{
+    enunciado: "pergunta2"
+    alternativas: ["alternativa1", "alternativas2"]
+}
+]
